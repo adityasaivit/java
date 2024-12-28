39443d8583796688eb5e66b961a3e35fa8abe2e0
adityasai <adityasaivit@gmail.com>
2024-12-28 15:11:38 +0530

    do while loop

---
+++ Dowhile.java
@@ -0,0 +1,14 @@
+public class Dowhile {
+
+    public static void main(String args[])
+    {   
+
+        int i=1;
+
+        do System.out.println(i);
+        while(++i<=10);
+        
+
+    }
+    
+}
