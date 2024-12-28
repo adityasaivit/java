674fc4c9cce440d31e0f2e2b72667b2e2353babb
adityasai <adityasaivit@gmail.com>
2024-12-28 19:21:16 +0530

    labels

---
+++ labels.java
@@ -0,0 +1,15 @@
+public class labels {
+    public static void main(String args[])
+    {
+        for(int i=1;i<=3;i++)
+        {
+            System.out.print(i);
+            for(int j=1;j<=5;j++)
+            {
+                System.out.println("\t"+j);
+            }
+            System.out.println("--------------------");
+        }
+    }
+    
+}
