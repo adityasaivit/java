0a497c9a83c1ccd1dcbe756a849088cfbbf31bc8
adityasai <adityasaivit@gmail.com>
2024-12-28 18:52:49 +0530

    switchcase string

---
+++ Switchstring.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Switchstring {
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        String option=sc.next();
+        switch(option)
+        {
+            case "monday":
+                System.out.println("Monday");
+                break;
+            case "tuesday":
+                System.out.println("Tuesday");
+                break;
+            case "wednesday":
+                System.out.println("Wednesday");
+                break;
+            case "thursday":
+                System.out.println("Thursday");
+                break;
+            case "friday":
+                System.out.println("Friday");
+                break;
+            case "saturday":
+                System.out.println("Saturday");
+                break;
+            case "sunday":
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("invalid input");
+        }
+        sc.close();
+    }
+    
+}
