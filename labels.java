f5d28ac76579e22d5659e7745e43d7dec0331cb9
adityasai <adityasaivit@gmail.com>
2024-12-28 19:22:09 +0530

    labels with labels included

--- labels.java
+++ labels.java
@@ -1,10 +1,10 @@
 public class labels {
     public static void main(String args[])
     {
-        for(int i=1;i<=3;i++)
+        label1: for(int i=1;i<=3;i++)
         {
             System.out.print(i);
-            for(int j=1;j<=5;j++)
+            label2: for(int j=1;j<=5;j++)
             {
                 System.out.println("\t"+j);
             }
