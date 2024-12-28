d24efd1e7ab653498ee2af7f84022ffe5850db0e
adityasai <adityasaivit@gmail.com>
2024-12-28 15:15:25 +0530

    dowhile

--- Dowhile.java
+++ Dowhile.java
@@ -7,7 +7,15 @@ public class Dowhile {
 
         do System.out.println(i);
         while(++i<=10);
-        
+
+
+        // second version
+        int j=1;
+        do{
+            System.out.println("second "+j);
+            j++;
+        }while(j<=100);
+
 
     }
     
